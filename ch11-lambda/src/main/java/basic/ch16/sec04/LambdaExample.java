a9da89ed4381305aef6fe4bbcf234ede227e618b
jionii <athena020219@naver.com>
2025-05-07 17:02:47 +0900

    0507-람다식 기본문제

--- ch11-lambda/src/main/java/basic/ch16/sec04/LambdaExample.java
+++ ch11-lambda/src/main/java/basic/ch16/sec04/LambdaExample.java
@@ -16,6 +16,19 @@ public class LambdaExample {
         });
     }
 
+    /*
+    * 메서드 참조
+    * 함수형 인터페이스를 람다식으로 쓰는 게 아닌, 일반 메서드를 참조시켜 선언하는 방법
+    * 일반 메서드를 참조하기 위한 조건
+    * 각 메서드의 매개변수 타입/갯수/반환형이 같아야 함
+    *
+    * // static
+    * 클래스이름::메서드이름
+    *
+    * // instance
+    * 참조변수이름::메서드이름
+    * */
+
     public static double sum(double x, double y) {
         return (x + y);
     }
